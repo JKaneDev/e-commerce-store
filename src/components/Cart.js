bcd183a4a35defeee5f498bb5d1723fd8fc172e7
James Kane <james.kane59@outlook.com>
2023-02-27 19:28:03 +0000

    additional cart styling, close on click

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -6,6 +6,7 @@ import Button from './Button';
 const Cart = ({ showCart, setShowCart }) => {
 	const cartRef = useRef(null);
 
+	// CLOSE CART BY CLICKING ON OTHER PART OF THE DOM
 	useEffect(() => {
 		const handleCloseCart = (e) => {
 			if (cartRef.current && !cartRef.current.contains(e.target)) {
@@ -16,33 +17,55 @@ const Cart = ({ showCart, setShowCart }) => {
 		return () => {
 			document.removeEventListener('mousedown', handleCloseCart);
 		};
-	}, [cartRef]);
+	}, [cartRef, setShowCart]);
+
+	// DARKEN DOM AND DISABLE SCROLLING WHILE CART OPEN
+	useEffect(() => {
+		document.body.style.overflow = 'hidden';
+
+		return () => {
+			document.body.style.overflow = 'auto';
+		};
+	}, [showCart]);
 
 	return (
-		<StyledCart>
-			<div>
-				<h3>No Games In Cart</h3>
-				<Button text='Clear' />
-			</div>
-			<div className='games-in-cart'></div>
-			<div className='footer'>
-				<span>Total: $0.00</span>
-				<button>
-					<p>Checkout</p>
-					<FaArrowRight size={14} />
-				</button>
-			</div>
-		</StyledCart>
+		<>
+			<Overlay />
+			<StyledCart ref={cartRef}>
+				<div>
+					<h3>No Games In Cart</h3>
+					<Button text='Clear' />
+				</div>
+				<div className='games-in-cart'></div>
+				<div className='footer'>
+					<span>Total: $0.00</span>
+					<button>
+						<p>Checkout</p>
+						<FaArrowRight size={14} />
+					</button>
+				</div>
+			</StyledCart>
+		</>
 	);
 };
 
 export default Cart;
 
+const Overlay = styled.div`
+	position: fixed;
+	top: 0;
+	left: 0;
+	width: 100vw;
+	height: 105vh;
+	background-color: rgba(0, 0, 0, 0.5);
+	z-index: 998;
+`;
+
 const StyledCart = styled.div`
 	display: flex;
 	flex-direction: column;
 	background-color: rgb(0, 0, 0, 0.9);
-	height: 100vh;
+	height: 101vh;
 	width: 325px;
 	z-index: 999;
 	position: fixed;
@@ -85,7 +108,7 @@ const StyledCart = styled.div`
 		margin-top: auto;
 		display: flex;
 		align-items: center;
-		padding: 2rem 2.5rem;
+		padding: 2rem 2rem 2.5rem 4rem;
 		display: flex;
 		align-items: center;
 
