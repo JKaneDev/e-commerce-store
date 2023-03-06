6c8464ae8a0a2ae71bae0fd0b27635f5b7a49922
James Kane <james.kane59@outlook.com>
2023-03-06 18:44:26 +0000

    hashHistory

--- src/index.js
+++ src/index.js
@@ -2,14 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { createHashHistory } from 'history';
-
 import GlobalStyle from './styles/GlobalStyle';
 import App from './App';
 
+const hashHistory = createHashHistory();
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-	<Router basename='/' history={hashHistory}>
-		<GlobalStyle />
-		<App />
-	</Router>
+	<React.StrictMode>
+		<Router basename='/' history={hashHistory}>
+			<GlobalStyle />
+			<App />
+		</Router>
+	</React.StrictMode>
 );
